52eeaf6ddd1d6237b258c5739d362ec401b566a3
Adrian Håkansson <adrian@adrianh.net>
2025-08-04 15:22:29 +0200

    Update to new HERE Maps API

--- geodle/core/services.py
+++ geodle/core/services.py
@@ -30,17 +30,8 @@ def getSatImage(lat, long, level=16, show_labels=False):
     """
     Returns a satellite image for a given location using the HERE API
     """
-
-    # Calculate the mercator projection values for the given lat and long
-    # TODO: Calculate and store this with gameround to save compute time?
-    latRad = lat * (math.pi / 180)
-    n = math.pow(2, level)
-    xTile = int((long + 180) / 360 * n)
-    yTile = int((1 - math.log(math.tan(latRad) + (1 / math.cos(latRad))) / math.pi) / 2 * n)
-
-    url = "https://1.aerial.maps.ls.hereapi.com/maptile/2.1/maptile/newest/{}.day/{}/{}/{}/512/jpg?apiKey={}".format(
-        'hybrid' if show_labels else 'satellite', level, xTile, yTile, HERE_API_KEY)
-
+    url = "https://image.maps.hereapi.com/mia/v3/{}/mc/center:{},{};zoom={}/512x512/jpeg?style=satellite.day&apiKey={}".format('base' if show_labels else 'background', lat, long, level, HERE_API_KEY)
+    print('Requesting sat image from url: {}', url)
     req = urllib.request.Request(url)
     response = urllib.request.urlopen(req)
     return response.read()
